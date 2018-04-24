5eee41bd2e4ea7ae86063582dbc93f8e247cb506
naonao <naonao@xizaideMacBook-Pro.local>
2018-04-24 20:12:15 +0800

    eureka server

--- miracle_service/src/main/java/com/miracle/service/MiracleServiceApplication.java
+++ miracle_service/src/main/java/com/miracle/service/MiracleServiceApplication.java
@@ -2,11 +2,13 @@ package com.miracle.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by naonao on 18/4/20.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class MiracleServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MiracleServiceApplication.class, args);
--- miracle_service/src/main/java/com/miracle/service/controllers/TestController.java
+++ miracle_service/src/main/java/com/miracle/service/controllers/TestController.java
@@ -1,8 +1,13 @@
 package com.miracle.service.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by naonao on 18/4/20.
@@ -10,8 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/miracle")
 public class TestController {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @RequestMapping(path = "/test", method = RequestMethod.GET)
     public String test() {
         return "miracle is naonao";
+    }
+
+    @RequestMapping(path = "/userInstances", method = RequestMethod.GET)
+    public List<ServiceInstance> showInfo() {
+        return discoveryClient.getInstances("MIRACLE-SERVICE");
     }
 }
